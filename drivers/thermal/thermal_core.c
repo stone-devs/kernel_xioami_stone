3f4946c4bfdab23f1866fccf79b7a49b0a2203b2
Nauval Rizky <enuma.alrizky@gmail.com>
2021-06-06 07:25:15 +0700

    thermal: Migrate to msm drm notifier
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -2004,23 +2004,19 @@ static void destroy_thermal_message_node(void)
 #ifdef CONFIG_DRM
 static int screen_state_for_thermal_callback(struct notifier_block *nb, unsigned long val, void *data)
 {
-	struct drm_notify_data *evdata = data;
+	struct msm_drm_notifier *evdata = data;
 	unsigned int blank;
 
-	if (val != DRM_EVENT_BLANK || !evdata || !evdata->data)
+	if (val != MSM_DRM_EVENT_BLANK || !evdata || !evdata->data)
 		return 0;
 
 	blank = *(int *)(evdata->data);
 	switch (blank) {
-	case DRM_BLANK_LP1:
-		pr_warn("%s: DRM_BLANK_LP1\n", __func__);
-	case DRM_BLANK_LP2:
-		pr_warn("%s: DRM_BLANK_LP2\n", __func__);
-	case DRM_BLANK_POWERDOWN:
+	case MSM_DRM_BLANK_POWERDOWN:
 		sm.screen_state = 0;
 		pr_warn("%s: DRM_BLANK_POWERDOWN\n", __func__);
 		break;
-	case DRM_BLANK_UNBLANK:
+	case MSM_DRM_BLANK_UNBLANK:
 		sm.screen_state = 1;
 		pr_warn("%s: DRM_BLANK_UNBLANK\n", __func__);
 		break;
@@ -2100,7 +2096,7 @@ static int __init thermal_init(void)
 #ifdef CONFIG_HQ_QGKI
 #ifdef CONFIG_DRM
 	sm.thermal_notifier.notifier_call = screen_state_for_thermal_callback;
-	if (drm_register_client(&sm.thermal_notifier) < 0) {
+	if (msm_drm_register_client(&sm.thermal_notifier) < 0) {
 		pr_warn("Thermal: register screen state callback failed\n");
 	}
 #endif
@@ -2126,7 +2122,9 @@ static void thermal_exit(void)
 {
 #ifdef CONFIG_HQ_QGKI
 #ifdef CONFIG_DRM
-	drm_unregister_client(&sm.thermal_notifier);
+#ifdef CONFIG_ARCH_QCOM
+	msm_drm_unregister_client(&sm.thermal_notifier);
+#endif
 #endif
 #endif
 	unregister_pm_notifier(&thermal_pm_nb);
